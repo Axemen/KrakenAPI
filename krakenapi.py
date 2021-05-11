d1d12aae90031611f95b7fb4b15edc452f670f02
Jonathan Randolph <Jrandolph011@gmail.com>
2021-05-11 11:26:33 -0500

    fixed the issue

--- krakenapi.py
+++ krakenapi.py
@@ -18,15 +18,7 @@ class KrakenAPI:
         if priv_key is None:
             self.priv_key = os.environ["KRAKEN_PRIVATE_KEY"]
 
-    def get_kraken_signature(urlpath, data, secret):
 
-        postdata = urllib.parse.urlencode(data)
-        encoded = (str(data['nonce']) + postdata).encode()
-        message = urlpath.encode() + hashlib.sha256(encoded).digest()
-
-        mac = hmac.new(base64.b64decode(secret), message, hashlib.sha512)
-        sigdigest = base64.b64encode(mac.digest())
-        return sigdigest.decode()
 
     def _create_api_signature(self, urlpath: str, data: dict) -> str:
         postdata = urllib.parse.urlencode(data)
@@ -42,7 +34,7 @@ class KrakenAPI:
 
     def _create_headers(self, endpoint: str, data: dict) -> dict:
         return {
-            "API-Key": self.pub_key,
+            "API-Key": "M6qUu7ezzlMLIo87QeW9Ssj7/lx8l1KlhvDoZ0rTOarSJ0u13FKPDTob",
             "API-Sign": self._create_api_signature(endpoint, data)
         }
 
@@ -52,31 +44,31 @@ class KrakenAPI:
 
     def _send_req(self, endpoint: str, data: dict) -> r.Response:
         headers = self._create_headers(endpoint, data)
-        return r.post(endpoint, headers=headers, data=data)
+        return r.post(self.base_url + endpoint, headers=headers, data=data)
 
     def get_balance(self) -> r.Response:
-        api_endpoint = self.base_url + "/0/private/Balance"
+        api_endpoint = "/0/private/Balance"
         data = self._create_data()
         print(data)
         return self._send_req(api_endpoint, data)
 
     def get_deposit_address(self, asset: str, method: str, new=True) -> r.Response:
-        api_endpoint = self.base_url + "/0/private/DepositAddresses"
+        api_endpoint = "/0/private/DepositAddresses"
         data = self._create_data(asset=asset, method=method, new=new)
         return self._send_req(api_endpoint, data)
 
     def get_trade_balance(self, asset: str) -> r.Response:
-        api_endpoint = self.base_url + "/0/private/TradeBalance"
+        api_endpoint = "/0/private/TradeBalance"
         data = self._create_data(asset=asset)
         return self._send_req(api_endpoint, data)
 
     def get_open_orders(self, trades=False, userref=None) -> r.Response:
-        api_endpoint = self.base_url + "/0/private/OpenOrders"
+        api_endpoint = "/0/private/OpenOrders"
         data = self._create_data(trades=trades, userref=userref)
         return self._send_req(api_endpoint, data)
 
     def get_closed_orders(self, trades=False, userref=None, start=None, end=None, ofs=None, closetime="both"):
-        api_endpoint = self.base_url + "/0/private/ClosedOrders"
+        api_endpoint = "/0/private/ClosedOrders"
         data = self._create_data(
             trades=trades, userref=userref, start=start, end=end, ofs=ofs, closetime=closetime)
         return self._send_req(api_endpoint, data)
@@ -84,6 +76,4 @@ class KrakenAPI:
 
 if __name__ == "__main__":
     api = KrakenAPI()
-    print(api.pub_key)
-    print(api.priv_key)
     print(api.get_balance().json())
