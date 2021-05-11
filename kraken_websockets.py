829871bfc5006e4f1b46f155a45b6944350fbcb0
Jonathan Randolph <Jrandolph011@gmail.com>
2021-05-11 17:15:19 -0500

    added basic websockets pattern. need to make it into class to produce websockets

---
+++ kraken_websockets.py
@@ -0,0 +1,36 @@
+import asyncio
+import json
+
+import websockets
+
+from krakenapi import KrakenAPI
+
+api = KrakenAPI()
+
+
+async def main():
+    uri = "wss://ws.kraken.com"
+
+    subscribe = {
+        "event": "subscribe",
+        "pair": [
+            "XBT/USD"
+        ],
+        "subscription": {
+            "name": "ticker"
+        }
+    }
+
+    async with websockets.connect(uri) as ws:
+        await ws.send(json.dumps(subscribe))
+        response = ws.recv()
+        print(response)
+        print('Connected')
+
+        while True:
+            data = await ws.recv()
+            print(data)
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main())
